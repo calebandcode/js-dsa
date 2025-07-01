f1abf119112cc531451e30bde2aca02c48533100
Caleb Akomaye <calebakomaye@outlook.com>
2025-07-01 16:41:14 +0100

    completed find max number challenge

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,15 @@
-function findMaxNumber() {}
+// const findMaxNumber = (arr) => {
+//   return Math.max(...arr);
+// };
+
+const findMaxNumber = (arr) => {
+  let max = arr[0];
+  for (i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+};
 
 module.exports = findMaxNumber;
