942d17081ae62fc959cea0cc1b5ba26cc98e3325
Caleb Akomaye <calebakomaye@outlook.com>
2025-07-01 15:30:26 +0100

    completed count occurence challenge

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,24 @@
-function countOccurrences() {}
+// const countOccurrences = (str, char) => {
+//   let count = 0;
+//   for (i = 0; i < str.length; i++) {
+//     if (str[i] === char) {
+//       count++;
+//     }
+//   }
+//   return count;
+// };
+
+// just trying something out 😊
+// const countOccurrences = (str, char) => {
+//   const count = str.split(char);
+//   console.log(str.split(char).length - 1, count);
+// };
+
+// countOccurrences('he
+// l
+// l
+// o', 'l'); --> he - - 0
+
+const countOccurrences = (str, char) => str.split(char).length - 1;
 
 module.exports = countOccurrences;
