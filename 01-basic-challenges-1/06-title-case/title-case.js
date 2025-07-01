b7de4b413d1c259b6c90c57e2cd7ed14afbfc880
Caleb Akomaye <calebakomaye@outlook.com>
2025-07-01 18:26:04 +0100

    complete reverse string challenge

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,9 @@
-function titleCase() {}
+const titleCase = (str) => {
+  let strArr = str.toLowerCase().split(' ');
+  for (i = 0; i < strArr.length; i++) {
+    strArr[i] = strArr[i][0].toUpperCase() + strArr[i].slice(1);
+  }
+  return strArr.join(' ');
+};
 
 module.exports = titleCase;
--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,20 @@
-function reverseString() {}
+const reverseString = (str) => {
+  return str.split('').reverse().join('');
+};
+
+// function reverseString(str) {
+//   let reversed = '';
+
+//   // Loop through the string backwards
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     // Add each character to the reversed string
+//     reversed += str[i];
+//   }
+
+//   // Return the reversed string
+//   return reversed;
+// }
+
+reverseString('hello');
 
 module.exports = reverseString;
