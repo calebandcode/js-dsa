a48276ba5db077dfa7ed0eb5e0b654598428ef17
Caleb Akomaye <akomayecaleb28@gmail.com>
2025-07-02 05:19:12 +0100

    completed fizbuzz and array intersection challenge

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,22 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  const arr = [];
 
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      arr.push('Fizz');
+    } else if (i % 5 === 0) {
+      arr.push('Buzz');
+    } else {
+      arr.push(i);
+    }
+  }
+
+  //   console.log(arr);
+
+  return arr;
+}
+
+// fizzBuzzArray(15);
 module.exports = fizzBuzzArray;
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,12 @@
-function arrayIntersection() {}
-
+function arrayIntersection(arr1, arr2) {
+  const intersect = [];
+  for (i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i]) && !intersect.includes(arr1[i])) {
+      intersect.push(arr1[i]);
+    }
+  }
+  //   console.log(intersect);
+  return intersect;
+}
+// arrayIntersection([1, 2, 2, 3, 3], [1, 2, 2, 3, 3, 6, 7]);
 module.exports = arrayIntersection;
