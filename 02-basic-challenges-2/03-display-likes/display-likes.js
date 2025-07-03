a6f85d49174e4f1721bdc00baf4ba3132121eebb
Caleb Akomaye <akomayecaleb28@gmail.com>
2025-07-03 16:13:35 +0100

    started and completed find missing letter challenge

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,13 @@
-function displayLikes() {}
+function displayLikes(names) {
+  if (names.length > 3) {
+    return `${names[0]}, ${names[1]}, ${names.length} others, likes this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]}, ${names[2]} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]}, ${names[1]}} likes this`;
+  } else if (names.length === 1) {
+    return `${names[0]}} others, likes this`;
+  }
+}
 
 module.exports = displayLikes;
--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,12 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  const n = arr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+  if (arr.length == 0) return undefined;
+  for (i = 0; i < arr.length; i++) {
+    actualSum += arr[i];
+  }
+  return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
